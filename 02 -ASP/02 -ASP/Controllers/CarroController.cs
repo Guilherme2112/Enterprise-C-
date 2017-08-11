43c9e2f549f458d42f3a0f49e8e333652019059c
logonrm <logonrm@fiap.com.br>
2017-08-11 11:53:22 -0300

    Razor e Helpers html

---
+++ 02 -ASP/02 -ASP/Controllers/CarroController.cs
@@ -0,0 +1,25 @@
+﻿using _02__ASP.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _02__ASP.Controllers
+{
+    public class CarroController : Controller
+    {
+        // GET: Carro
+        [HttpGet]
+        public ActionResult Cadastrar()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Cadastrar(Carro carro)
+        {
+            return View();
+        }
+
+    }
+}
---
+++ 02 -ASP/02 -ASP/Models/Carro.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _02__ASP.Models
+{
+    public class Carro
+    {
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public string Placa { get; set; }
+        public int Ano{get;set; }
+        public decimal Preco { get; set; }
+
+
+    }
+}
